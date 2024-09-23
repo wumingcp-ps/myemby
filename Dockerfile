19b0ca29fe537bdd552b93d03e3e25585eff9a0e
wumingcp <86212949+wumingcp-ps@users.noreply.github.com>
2024-09-23 14:52:45 +0800

    add all

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM emby/embyserver:latest
+
+COPY run.sh run.sh
+
+RUN chmod +x run.sh
+
+CMD [ "/run.sh" ]
---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "Hello, this is a script running in Docker!"
+
+if [ -z "$SMB_USER" ] || [ -z "$SMB_PASS" ] || [ -z "$SMB_SERVER" ] || [ -z "$SMB_SHARE" ]; then
+  echo "One or more required environment variables are missing:"
+  echo "SMB_USER, SMB_PASS, SMB_SERVER, SMB_SHARE"
+  exit 1
+fi
+
+echo "Mount the qnap using environment variables"
+mkdir /qnap
+mount -t cifs //$SMB_SERVER/$SMB_SHARE /qnap -o username=$SMB_USER,password=$SMB_PASS,vers=3.0
+
+if [ $? -eq 0 ]; then
+  echo "Mounted qnap successfully."
+else
+  echo "Failed to mount qnap."
+  exit 1
+fi
+
+/init
